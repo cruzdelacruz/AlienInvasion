6136e311d7b1f350e414b5c8c2387e9f597fee95
Cruz <cruz.delacruz37@gmail.com>
2019-06-11 15:02:51 -0400

    added settings module. added images folder

---
+++ alien_invasion.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+import sys
+import pygame
+from settings import Settings
+
+def run_game():
+    # initialize pygame, settings and create a screen object.
+    pygame.init()
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+            (ai_settings.screen_width, ai_settings.screen_height))
+    pygame.display.set_caption("Alien Invasion")
+
+   
+   # start the main loop for the game.
+    while True:
+
+        # watch for keyboard  and mouse events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+        # redraw the screen during each pass through the loop
+        screen.fill(ai_settings.bg_color)
+
+        # make the most recently drawn screen visibile.
+        pygame.display.flip()
+
+run_game()
+
+
